0487383b2463e8c4fce64bba44e46c5859b08199
Siva <sivaksr7026@gmail.com>
2026-01-04 23:33:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.json())
     app.use(cors({
-  origin: ["https://shooping.azurewebsites.net"],
+  origin: ["flipcartshopping.azurewebsites.net"],
    methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
