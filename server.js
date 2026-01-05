a8972e51a422c4fc97ce036d4699b4cb3b44c7f2
Siva <sivaksr7026@gmail.com>
2026-01-05 11:58:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,18 +11,16 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(express.json())
-  app.use(
+app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "https://shoppingapp.azurewebsites.net"
-    ],
+    origin: ["https://shoppingapp.azurewebsites.net"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: false
   })
 );
+
+app.options("*", cors());
 
 
 mongoose.connect(process.env.MONGO_URI)
