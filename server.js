ec701950fc44729af287c6886af4e5eec1a2bdc4
Siva <sivaksr7026@gmail.com>
2026-01-05 12:28:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,13 +10,14 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
+app.use(express.json());
 
 app.use(
   cors({
     origin: ["https://shoppingapp.azurewebsites.net"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
+    credentials: false
   })
 );
 
