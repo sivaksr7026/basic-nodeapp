d7388a1156dd864e60ef3234928e6d84430ee2cc
Siva <sivaksr7026@gmail.com>
2026-01-05 17:16:33 +0530

    commit code

--- server.js
+++ server.js
@@ -13,30 +13,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 
-app.use(
-  cors({
-    origin: ["https://shoppingapp.azurewebsites.net"],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: false
-  })
-);
-
-// Define the specific allowed origins
-const allowedOrigins = ['https://shoppingapp.azurewebsites.net/', 'http://localhost:3000'];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests) or from the allowed list
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-};
-
-app.use(cors(corsOptions));
+app.use(cors({origin:"https://shoppingapp.azurewebsites.net"}))
 
 
 
@@ -48,10 +25,7 @@ mongoose.connect(process.env.MONGO_URI)
     console.error('MongoDB connection error:', error)
   })
 
-
-
 app.use('/employees', employeeRoutes)
-
 
 
  app.get('/', (req, res) => {
